7ed3074186deb820d6d783fb0b40c2b5ababcaf6
Roshan Lamichhane <lamichhanersn@gmail.com>
2020-02-22 17:32:42 +0545

    Added more operations!

--- stack_using_linked_list.cpp
+++ stack_using_linked_list.cpp
@@ -1,18 +1,25 @@
-//A basic stack implementation using linked list
-
-#include <iostream>
+//stack implementation using linked list
+//stack to store int values
+#include<iostream>
+using namespace std;
 
 class node
-{                                     
+{               //node to create linked list
     public:
-    int value;                //to hold some value
-    node *link;               //class pointer to link one node with other
+    int value;
+    node *link;
 };
 
-node *top=NULL;           
-node *tempNode;
+node *top=NULL;  //store the address of topmost node in the stack
+node *tempNode;  //a temporary node
 
-node *createNode(int x)
+int isEmpty()  //function to check if the stack is empty
+{
+    if(top==NULL) return 1;
+    else return 0;
+}
+
+node *createNode(int x) //function to create a new node
 {
     tempNode = new node;
     tempNode->value = x;
@@ -20,9 +27,10 @@ node *createNode(int x)
     return tempNode;
 }
 
-void push(node *x)
+void push(node *x) //function to insert an item into the stack
 {
-    if(top==NULL)
+
+ if(top==NULL)
     {
         top = x;
         top->link=NULL;
@@ -34,11 +42,11 @@ void push(node *x)
     }
 }
 
-void pop()
+void pop() //function to remove the topmost item from stack
 {
-    if(top==NULL)
+    if(isEmpty() )
     {
-        std::cout<<"Illegal operation: Stack Underflow"<<std::endl;
+        cout<<"Illegal operation: Stack Underflow"<<endl;
     }
     else
     {
@@ -48,38 +56,80 @@ void pop()
     }
 }
 
-void displayStack()
+void displayStack() //traverse and display each item of stack
 {
     tempNode = top;
-    if(tempNode == NULL)
+    if(isEmpty() == 1)
     {
-        std::cout<<"Illegal operation: Empty Stack!"<<std::endl;
+        cout<<"Illegal operation: Empty Stack!"<<endl;
     }
     else
     {
         while(tempNode!=NULL)
         {
-            std::cout<<tempNode->value<<"\t";
+            cout<<tempNode->value<<"\t";
             tempNode = tempNode->link;
         }
     }
 
 }
 
+                    //function to delete all items of stack,
+void deleteAll()    // mainly used to delete the dynamically created nodes at the time of program exit
+{
+	if(!isEmpty())
+	{
+	    tempNode = top;
+        while(tempNode!=NULL)
+        {
+        top=top->link;
+        delete tempNode;
+        tempNode=top;
+        }
+	}
+}
+
+void search() //function to search for an specific item in the stack
+{
+    if(isEmpty())
+    {
+        cout<<"Illegal operation: Stack underflow!"<<endl;
+    }
+    else
+    {
+        int t,c;
+        cout<<"Enter item to search for :";
+        cin >>t;
+        while(tempNode!=NULL)
+        {
+            if(t == tempNode->value)
+            {
+                cout<< "Item found!"<<endl;
+                c=1;
+                break;
+            }
+            tempNode = tempNode->link;
+            c=0;
+        }
+        if(c==0) cout<<"Item not found !"<<endl;
+    }
+}
 int main()
 {
-    int n,val;
-    node *y;    //a temporary node
+    int n;
+    int val;
+    node *y;
     while(1)
     {
-        std::cout<<std::endl<<"1.push\n2.pop\n3.Display all items\n4.exit"<<std::endl;
-        std::cout<<"Enter a option: ";
-        std::cin>>n;
+
+        cout<<endl<<"1.push\n2.pop\n3.Display all items\n4.isEmpty()\n5.Search\n6.Exit"<<endl;
+        cout<<"Enter a option: ";
+        cin>>n;
         switch(n)
         {
             case 1:
-               std::cout<<"\nEnter a value: ";
-               std::cin>>val;
+                cout<<"\nEnter a value: ";
+                cin>>val;
                 y = createNode(val);
                 push(y);
                 break;
@@ -91,8 +141,16 @@ int main()
             case 3:
                 displayStack();
                 break;
-
             case 4:
+                if(isEmpty() == 1) cout<<"\n The stack is empty !\n";
+                else cout<<"\nThe stack is not empty!\n";
+                break;
+            case 5:
+                search();
+                break;
+
+            case 6:
+                deleteAll(); //calling this function is necessary, otherwise not deleting dynamically created nodes may cause memory leak
                 exit(0);
         }
     }
